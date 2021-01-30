65bb5f8f18b88f0f278c8505b9903d920ee79857
KOUSHIK SAHA <koushik.official1999@gmail.com>
2021-01-30 16:01:01 +0530

    Add project files.

---
+++ FirstAppRaspberryPi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace FirstAppRaspberryPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int LedPin = 4;
+            GpioController controller = new GpioController();
+            controller.OpenPin(LedPin, PinMode.Output);
+
+
+            while (true)
+            {
+                controller.Write(LedPin, PinValue.High);
+                Thread.Sleep(2000);
+                controller.Write(LedPin, PinValue.Low);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
